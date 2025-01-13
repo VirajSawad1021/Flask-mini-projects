b8a4e22b1eeaf31820c697f72a695da112d458d6
Viraj Sawad <virajsawad1021@gmail.com>
2025-01-13 21:02:29 +0545

    Update forms.py

--- Expenzy-Expense_Tracker/Expense/app/forms.py
+++ Expenzy-Expense_Tracker/Expense/app/forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, FloatField, DateField, SubmitField
 from wtforms.validators import DataRequired
 
 class ExpenseForm(FlaskForm):
-    name = StringField('Name,', validators=[DataRequired()])
+    name = StringField('Name', validators=[DataRequired()])
     category = StringField('Category', validators=[DataRequired()])
     amount = FloatField('Amount', validators=[DataRequired()])
     date = DateField('Date', format='%Y-%m-%d', validators=[DataRequired()])
